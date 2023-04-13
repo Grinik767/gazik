ff595e4e4747d014fdaf6dc289185770f5e6f132
Grinik767 <grigorikiseleff@yandex.ru>
2023-04-13 22:17:18 +0500

    bug fix

--- main.py
+++ main.py
@@ -68,10 +68,10 @@ class App(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.have_photo = None
+
         self.draw_objects = Drawing(QColor(0, 128, 0))
         self.draw_def = Drawing(QColor(0, 128, 0))
-        self.gridLayout_4.addWidget(self.draw_objects)
-        self.gridLayout_2.addWidget(self.draw_def)
 
         self.pushButton_3.clicked.connect(self.save_images)
         self.pushButton.clicked.connect(self.clear_objects)
@@ -87,20 +87,22 @@ class App(QMainWindow, Ui_MainWindow):
         self.colorButton_7.clicked.connect(lambda: self.draw_def.change_color(QColor(255, 0, 0)))
 
     def clear_objects(self):
-        while self.gridLayout_4.count():
-            child = self.gridLayout_4.takeAt(0)
-            if child.widget():
-                child.widget().deleteLater()
-        self.draw_objects = Drawing(QColor(0, 128, 0))
-        self.gridLayout_4.addWidget(self.draw_objects)
+        if self.have_photo:
+            while self.gridLayout_4.count():
+                child = self.gridLayout_4.takeAt(0)
+                if child.widget():
+                    child.widget().deleteLater()
+            self.draw_objects = Drawing(QColor(0, 128, 0))
+            self.gridLayout_4.addWidget(self.draw_objects)
 
     def clear_defects(self):
-        while self.gridLayout_2.count():
-            child = self.gridLayout_2.takeAt(0)
-            if child.widget():
-                child.widget().deleteLater()
-        self.draw_def = Drawing(QColor(0, 128, 0))
-        self.gridLayout_2.addWidget(self.draw_def)
+        if self.have_photo:
+            while self.gridLayout_2.count():
+                child = self.gridLayout_2.takeAt(0)
+                if child.widget():
+                    child.widget().deleteLater()
+            self.draw_def = Drawing(QColor(0, 128, 0))
+            self.gridLayout_2.addWidget(self.draw_def)
 
     def upload_photo(self):
         try:
@@ -108,34 +110,38 @@ class App(QMainWindow, Ui_MainWindow):
                                                     'Изображение (*.jpg *.png);')[0][0]
             image = Image.open(filename).resize((1024, 64))
             image.save('image_start.png')
+            self.have_photo = True
+            self.gridLayout_4.addWidget(self.draw_objects)
+            self.gridLayout_2.addWidget(self.draw_def)
             self.canvas_8.setPixmap(QPixmap('image_start.png'))
         except Exception as E:
             pass
 
     def save_images(self):
-        try:
-            filename = \
-                QFileDialog.getSaveFileName(self, "Сохранить изображения", '',
-                                            'Изображение (*.png);;Изображение (*.jpg);')[
-                    0]
-            type_of_f = filename.split('/')[-1].split('.')[-1]
-            self.draw_objects.save_as_image(f'objects_1.{type_of_f}')
-            self.draw_def.save_as_image(f'defects_1.{type_of_f}')
-            image_obj = Image.open(f'objects_1.{type_of_f}')
-            image_def = Image.open(f'defects_1.{type_of_f}')
-            image_start = Image.open(f'image_start.png')
-            image_object_size = image_obj.size
-            new_image = Image.new('RGB', (image_object_size[0], 3 * image_object_size[1] + 3), (255, 255, 255))
-            new_image.paste(image_start, (0, 0))
-            new_image.paste(image_obj, (0, image_object_size[1] + 1))
-            new_image.paste(image_def, (0, 2 * image_object_size[1] + 2))
-            new_image.save(filename)
-            new_image.show()
-            os.remove(f'objects_1.{type_of_f}')
-            os.remove(f'defects_1.{type_of_f}')
-            os.remove('image_start.png')
-        except Exception as e:
-            pass
+        if self.have_photo:
+            try:
+                filename = \
+                    QFileDialog.getSaveFileName(self, "Сохранить изображения", '',
+                                                'Изображение (*.png);;Изображение (*.jpg);')[
+                        0]
+                type_of_f = filename.split('/')[-1].split('.')[-1]
+                self.draw_objects.save_as_image(f'objects_1.{type_of_f}')
+                self.draw_def.save_as_image(f'defects_1.{type_of_f}')
+                image_obj = Image.open(f'objects_1.{type_of_f}')
+                image_def = Image.open(f'defects_1.{type_of_f}')
+                image_start = Image.open(f'image_start.png')
+                image_object_size = image_obj.size
+                new_image = Image.new('RGB', (image_object_size[0], 3 * image_object_size[1] + 3), (255, 255, 255))
+                new_image.paste(image_start, (0, 0))
+                new_image.paste(image_obj, (0, image_object_size[1] + 1))
+                new_image.paste(image_def, (0, 2 * image_object_size[1] + 2))
+                new_image.save(filename)
+                new_image.show()
+                os.remove(f'objects_1.{type_of_f}')
+                os.remove(f'defects_1.{type_of_f}')
+                os.remove('image_start.png')
+            except Exception as e:
+                pass
 
 
 def except_hook(cls, exception, traceback):
